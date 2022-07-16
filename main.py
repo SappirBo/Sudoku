2bfc048d9244aaadef04ecd30b98a766b6e9ca5f
Sappir Bohbot <sappir.bohbot@msmail.ariel.ac.il>
2022-07-16 12:02:48 +0300

    Game v.1.3

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ def run():
         os.system('cls')
         print(" ")
         current_game.print_game()
-        next_move_x = int(input("next x?"))
-        next_move_y = int(input("next y?"))
-        next_move_number = int(input("number?"))
-        current_game.move(next_move_x,next_move_y,next_move_number)
+        see_ans = input("Do Yo Wish To See Solution? (yes = 1) ")
+        if see_ans == "1":
+            current_game.board_partial.board = current_game.board_full.board
+            os.system('cls')
+            print(" ")
+            current_game.print_game()
+        else:
+            next_move_x = int(input("next x?"))
+            next_move_y = int(input("next y?"))
+            next_move_number = int(input("number?"))
+            current_game.move(next_move_x,next_move_y,next_move_number)
 
 
 if __name__ == "__main__":
