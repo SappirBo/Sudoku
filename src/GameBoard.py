d6d872c4ba34a15fb7f3f65e1819ffa2867d6f5a
Sappir Bohbot <sappir.bohbot@msmail.ariel.ac.il>
2022-07-16 10:20:07 +0300

    GameBoard v.1.2

--- src/GameBoard.py
+++ src/GameBoard.py
@@ -1,31 +1,64 @@
 import random
-
+from random import shuffle
 BOARD_SIZE = 9
-table = [1,2,3,4,5,6,7,8,9]
+table = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+shuffle(table)
 
 class GameBoard:
     def __init__(self):
-        self.board = [[0 for x in range(BOARD_SIZE)] for y in range(BOARD_SIZE) ]
+        self.board = [[0 for x in range(BOARD_SIZE)] for y in range(BOARD_SIZE)]
         self.create_board()
 
     def create_board(self):
         for i in range(0, 9):
-            for j in range(0,9):
+            for j in range(0, 9):
                 self.board[i][j] = 0
 
-    def at(self,row: int, column: int) -> int:
+    def check_board(self) -> bool:
+        for i in range(0, 9):
+            for j in range(0, 9):
+                if self.board[i][j] != 0:
+                    return False
+
+    def at(self, row: int, column: int) -> int:
         return self.board[row][column]
 
-    def fill_board(self):
-        tmp_table = table
-        for i in range(0,3):
-            for j in range(0,3):
-                num = random.randint(0,9)
-                while num not in tmp_table:
-                    num = random.randint(0, 9)
-                self.board[i][j] = num
-                tmp_table.remove(num)
+    def possible(self, x, y, n) -> bool:
+        # check row and column.
+        for i in range(9):
+            if self.board[x][i] == n:
+                return False
+            if self.board[i][y] == n:
+                return False
+        # checking the 3X3 table of this position.
+        x0 = (x // 3) * 3
+        y0 = (y // 3) * 3
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.board[x0 + i][y0 + j] == n:
+                    return False
+        return True
 
+    def ad_numbers(self):
+        for i in range(30):
+            i = random.randint(0,8)
+            j = random.randint(0,8)
+            for number in table:
+                if self.possible(i, j, number):
+                    self.board[i][j] = number
+
+
+    def fill_board(self):
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] == 0:
+                    for number in table:
+                        if self.possible(i, j, number):
+                            self.board[i][j] = number
+                            self.fill_board()
+                            self.board[i][j] = 0
+                    return
+        print("Done")
 
     def print_board(self):
         for line in self.board:
--- src/game.py
+++ src/game.py
@@ -2,9 +2,7 @@ from GameBoard import *
 from table import *
 
 g = GameBoard()
+g.ad_numbers()
+g.print_board()
 g.fill_board()
 g.print_board()
-
-
-# tbl = table(3)
-# tbl.print_table()
