c335256d32a0d04801580f96ebf115fd3595622b
Sappir Bohbot <sappir.bohbot@msmail.ariel.ac.il>
2022-07-16 19:55:08 +0300

    GUI V.1.3

--- GUI/game_gui.py
+++ GUI/game_gui.py
@@ -6,6 +6,7 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from src.game import *
 from src.GameBoard import *
+import time
 
 # Setting up size of the up
 Window.size = (800, 400)
@@ -20,13 +21,16 @@ class myLayout(Widget):
     def clear_board(self):
         for row in range(9):
             for col in range(9):
-                    row_str = str(row)
-                    col_str = str(col)
-                    key = "n" + row_str + col_str
-                    self.ids[key].text = ""
+                row_str = str(row)
+                col_str = str(col)
+                key = "n" + row_str + col_str
+                self.ids[key].text = ""
 
     def start_game_easy(self):
-        curr_game = game(1)
+        curr_game.reAnitioliz_game()
+        self.draw_board()
+
+    def draw_board(self):
         self.clear_board()
         for row in range(9):
             for col in range(9):
@@ -36,8 +40,38 @@ class myLayout(Widget):
                     key = "n" + row_str + col_str
                     self.ids[key].text = str(curr_game.board_partial.board[row][col])
 
-    # button pressing function
-    # def move(self, button):
+    def move(self):
+        for row in range(9):
+            for col in range(9):
+                if curr_game.board_partial.board[row][col] == 0:
+                    row_str = str(row)
+                    col_str = str(col)
+                    key = "n" + row_str + col_str
+                    input_number = self.ids[key].text
+                    if input_number != '':
+                        input_number = int(input_number)
+                        if curr_game.board_partial.possible(row, col, input_number):
+                            print("Good Move")
+                            curr_game.board_partial.board[row][col] = input_number
+                        else:
+                            print("Bad Move")
+        self.draw_board()
+
+    def reStart(self):
+        print("__ReStart__")
+        curr_game.board_partial_copy.print_board()
+        curr_game.reAnitioliz_game()
+        curr_game.board_partial.print_board()
+        self.draw_board()
+
+    def answer(self):
+        self.clear_board()
+        for row in range(9):
+            for col in range(9):
+                row_str = str(row)
+                col_str = str(col)
+                key = "n" + row_str + col_str
+                self.ids[key].text = str(curr_game.board_full.board[row][col])
 
 
 
--- src/game.py
+++ src/game.py
@@ -1,32 +1,38 @@
 from src.GameBoard import *
 import random
 
+
 class game:
     def __init__(self, game_difficulty: int):
         self.difficulty = int(game_difficulty)
         self.board_full = GameBoard()
         self.board_partial = GameBoard()
-
+        self.board_partial_copy = GameBoard()
         self.initiolize_game()
 
     def initiolize_game(self):
         self.board_full.fill_board()
-        filled_slots = int(30/self.difficulty)
+        filled_slots = int(30 / self.difficulty)
         for times in range(filled_slots):
             i = random.randint(0, 8)
             j = random.randint(0, 8)
             self.board_partial.board[i][j] = self.board_full.board[i][j]
+        self.board_partial_copy.board = self.board_partial.board.copy()
 
-    def move(self,row: int, column: int ,number: int ):
+    def reAnitioliz_game(self):
+        self.board_partial.board.clear()
+        self.board_partial.board = self.board_partial_copy.board.copy()
+
+    def move(self, row: int, column: int, number: int):
         if number >= 10 or number <= 0:
             raise Exception("Invalid Number")
-        if row >=10 or row <= 0:
+        if row >= 10 or row <= 0:
             raise Exception("Invalid Row")
-        if column >=10 or column <= 0:
+        if column >= 10 or column <= 0:
             raise Exception("Invalid Column")
         row -= 1
         column -= 1
-        if self.board_partial.board[row][column] == 0 and self.board_partial.possible(row,column,number):
+        if self.board_partial.board[row][column] == 0 and self.board_partial.possible(row, column, number):
             self.board_partial.board[row][column] = number
 
     def game_stats(self):
@@ -38,4 +44,3 @@ class game:
 
     def print_game(self):
         self.board_partial.print_board()
-
